c65d30834b00295598130314ed8c102ea90da486
dvalvezon <danilovalvezon@hotmail.com>
2013-11-07 13:35:39 -0200

    Returning a Proxy from EM creator...

--- ultrashare/src/main/java/com/ultrashare/factory/EntityManagerCreator.java
+++ ultrashare/src/main/java/com/ultrashare/factory/EntityManagerCreator.java
@@ -1,5 +1,6 @@
 package com.ultrashare.factory;
 
+import javax.annotation.PreDestroy;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -8,28 +9,21 @@ import br.com.caelum.vraptor.ioc.ApplicationScoped;
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.caelum.vraptor.ioc.ComponentFactory;
 
+import com.ultrashare.component.facilities.EntityManagerProxy;
+
 @Component
 @ApplicationScoped
 public class EntityManagerCreator implements ComponentFactory<EntityManager> {
 
 	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
 
-	// private EntityManager em;
-
-	// @PostConstruct
-	// private void create() {
-	// em = emf.createEntityManager();
-	// }
-
-	// @PreDestroy
-	// private void destroy() {
-	// em.close();
-	// emf.close();
-	// }
+	@PreDestroy
+	private void closeEntityManagerFactoryOnApplicationShutdown() {
+		emf.close();
+	}
 
 	public EntityManager getInstance() {
-		// return em;
-		return emf.createEntityManager();
+		return new EntityManagerProxy(emf);
 	}
 
 }
