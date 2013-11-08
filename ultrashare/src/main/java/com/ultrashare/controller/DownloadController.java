a183423b9236a3cf2bbb7e42f676c82081a0a45a
dvalvezon <danilovalvezon@hotmail.com>
2013-11-08 21:53:27 -0200

    Added functionality in DownloadController...
    //TODO - Do not forget to remove test method...

--- ultrashare/src/main/java/com/ultrashare/controller/DownloadController.java
+++ ultrashare/src/main/java/com/ultrashare/controller/DownloadController.java
@@ -1,7 +1,5 @@
 package com.ultrashare.controller;
 
-import java.io.InputStream;
-
 import org.apache.log4j.Logger;
 
 import br.com.caelum.vraptor.Get;
@@ -14,6 +12,7 @@ import br.com.caelum.vraptor.interceptor.download.InputStreamDownload;
 import com.ultrashare.component.facilities.FTPHandler;
 import com.ultrashare.component.facilities.FTPRetrieveAction;
 import com.ultrashare.component.facilities.Validate;
+import com.ultrashare.dao.ShareDAO;
 import com.ultrashare.model.Share;
 
 @Resource
@@ -23,31 +22,52 @@ public class DownloadController {
 
 	private Result result;
 
-	public DownloadController(Result result) {
+	private ShareDAO shareDao;
+
+	public DownloadController(Result result, ShareDAO shareDao) {
 		this.result = result;
+		this.shareDao = shareDao;
 	}
 
 	@Get
-	@Path("/upload/confirm/{id}/{confirmationCode}")
+	@Path("/download/request/{id}/{confirmationCode}")
 	public void request(String id, String confirmationCode) {
 		if (!Validate.ifAnyStringIsNullOrEmpty(id, confirmationCode) && !Validate.ifAnyStringIsNotNumeric(id, confirmationCode)) {
-			result.redirectTo(this).proceed();
-			return;
+			Share share = shareDao.find(Long.valueOf(id));
+			if (share != null && share.getConfirmationCode().equals(Long.valueOf(confirmationCode))) {
+				result.redirectTo(this).confirm(share);
+				return;
+			}
 		}
 		result.redirectTo(UploadController.class).form();
 	}
 
-	public void proceed() {
+	public Share confirm(Share share) {
+		return share;
+	}
 
+	// pi -> Share id, pc -> Share confirmationCode
+	public void start(String pi, String pc) {
+		if (!Validate.ifAnyStringIsNullOrEmpty(pi, pc) && !Validate.ifAnyStringIsNotNumeric(pi, pc)) {
+			Share share = shareDao.find(Long.valueOf(pi));
+			if (share != null && share.getConfirmationCode().equals(Long.valueOf(pc))) {
+				share.addDownloadAttempt();
+				shareDao.update(share);
+				result.redirectTo(this).download(share);
+				return;
+			}
+		}
+		result.redirectTo(UploadController.class).form();
 	}
 
 	@Get
-	@Path("/upload/test/{fileName}")
+	@Path("/download/test/{fileName}")
 	public Download test(String fileName) {
 		return new InputStreamDownload(FTPHandler.processFTPAction(new FTPRetrieveAction(fileName)), "application/octet-stream", "test.txt");
 	}
 
-	public Download download(Share share, InputStream inputStream) {
-		return new InputStreamDownload(inputStream, "application/binary", share.getSharedUpload().getFileName(), true, 104857600);
+	private Download download(Share share) {
+		return new InputStreamDownload(FTPHandler.processFTPAction(new FTPRetrieveAction(share.getSharedUpload().getFileName())), share.getSharedUpload()
+				.getFileContentType(), share.getSharedUpload().getFileName(), true, share.getSharedUpload().getFileSize());
 	}
 }
