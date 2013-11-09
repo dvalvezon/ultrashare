7d9a05cbfcf2c5315ef6133aaa33e7f2e8ab466c
Danilo Valvezon <danilovalvezon@hotmail.com>
2013-11-09 21:31:55 -0200

    Confirmation Proccessor and VO implemented

--- ultrashare/src/main/java/com/ultrashare/component/vo/ConfirmationVO.java
+++ ultrashare/src/main/java/com/ultrashare/component/vo/ConfirmationVO.java
@@ -1,5 +1,19 @@
 package com.ultrashare.component.vo;
 
+import java.util.Collections;
+import java.util.List;
+
+import com.ultrashare.model.Share;
+
 public class ConfirmationVO {
 
+	private final List<Share> shareList;
+
+	public ConfirmationVO(List<Share> shareList) {
+		this.shareList = Collections.unmodifiableList(shareList);
+	}
+
+	public List<Share> getUnmodifiableShareList() {
+		return this.shareList;
+	}
 }
