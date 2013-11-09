9a8527b8ea43c308af793f0e2ec2ba28f1f255a1
dvalvezon <danilovalvezon@hotmail.com>
2013-11-08 22:01:41 -0200

    Hard changes to make easier the task of sending multiple emails

--- ultrashare/src/main/java/com/ultrashare/component/facilities/MailSender.java
+++ ultrashare/src/main/java/com/ultrashare/component/facilities/MailSender.java
@@ -1,10 +1,12 @@
 package com.ultrashare.component.facilities;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.Address;
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -13,81 +15,125 @@ import javax.mail.internet.MimeMessage;
 
 import org.apache.log4j.Logger;
 
+import com.ultrashare.component.vo.MailVO;
 import com.ultrashare.type.PropertyType;
 
 public final class MailSender {
 
 	private static final Logger logger = Logger.getLogger(MailSender.class);
 
+	private static final Properties emailProperties;
+	static {
+		try {
+			emailProperties = PropertyLoader.loadProperties(PropertyType.MAIL_CONFIGS);
+		} catch (Exception e) {
+			logger.error("Could not load Email Properties...", e);
+			throw new RuntimeException(e);
+		}
+	}
+
 	private MailSender() {
 
 	}
 
-	public static void sendMail(String[] recipients, String mailSubject, String mailText) {
-		logger.debug("Begin of method public sendMail().");
-		logger.debug("Recipients: " + recipients != null ? Arrays.deepToString(recipients) : "[null]");
-		logger.debug("Subject: " + mailSubject != null ? mailSubject : "[null]");
-		logger.debug("Mail Body: " + mailText != null ? mailText : "[null]");
-		try {
-			logger.debug("Building mails...");
-			Transport.send(createMessage(recipients, mailSubject, mailText));
-			logger.debug("Mail has been sent.");
-		} catch (Exception e) {
-			logger.error("Could not send mails.", e);
-			throw new RuntimeException(e);
-		} finally {
-			logger.debug("End of method public sendMail().");
+	// public static void sendMail(String[] recipients, String mailSubject,
+	// String mailText) {
+	// logger.debug("Begin of method public sendMail().");
+	// logger.debug("Recipients: " + recipients != null ?
+	// Arrays.deepToString(recipients) : "[null]");
+	// logger.debug("Subject: " + mailSubject != null ? mailSubject : "[null]");
+	// logger.debug("Mail Body: " + mailText != null ? mailText : "[null]");
+	// try {
+	// logger.debug("Building mails...");
+	// Transport.send(createMessage(recipients, mailSubject, mailText));
+	// logger.debug("Mail has been sent.");
+	// } catch (Exception e) {
+	// logger.error("Could not send mails.", e);
+	// throw new RuntimeException(e);
+	// } finally {
+	// logger.debug("End of method public sendMail().");
+	// }
+	// }
+
+	public static void sendMails(List<MailVO> mails) {
+		List<Message> messages = new ArrayList<Message>();
+		Session emailSession = getMailSession();
+		for (MailVO mail : mails) {
+			try {
+				messages.add(createMessage(mail, emailSession));
+			} catch (MessagingException e) {
+				logger.error("Could not create message with VO:" + mail.toString(), e);
+			}
+		}
+		for (Message message : messages) {
+			try {
+				Transport.send(message);
+			} catch (MessagingException e) {
+				logger.error("Could not send message: " + message.toString(), e);
+			}
 		}
 	}
 
-	private static Message createMessage(String[] recipients, String mailSubject, String mailText) throws Exception {
-		logger.debug("Creating the message...");
-		logger.debug("Retrieving mail properties...");
-		Properties mailProperties = getMailProperties();
-		logger.debug("Properties found: " + mailProperties.toString());
-		String senderMail = getSenderMail(mailProperties);
-		String senderPassword = getSenderPassword(mailProperties);
-
-		logger.debug("Building message...");
-		Message message = new MimeMessage(getMailSession(mailProperties, senderMail, senderPassword));
-		logger.debug("Setting FROM field with " + senderMail);
-		message.setFrom(new InternetAddress(senderMail));
-		logger.debug("Parsing recipients...");
-		Address[] userMails = InternetAddress.parse(concatRecipients(recipients));
-		logger.debug("Setting TO field with " + Arrays.deepToString(userMails));
+	private static Message createMessage(MailVO mail, Session emailSession) throws MessagingException {
+		Message message = new MimeMessage(emailSession);
+		message.setFrom(new InternetAddress(getSenderMail()));
+		Address[] userMails = InternetAddress.parse(concatRecipients(mail.getRecipients()));
 		message.setRecipients(Message.RecipientType.TO, userMails);
-		logger.debug("Setting SUBJECT field with \"" + mailSubject + "\".");
-		message.setSubject(mailSubject);
-		logger.debug("Setting Mail Text with \"" + mailText + "\".");
-		message.setText(mailText);
-		logger.debug("Message has been created.");
+		message.setSubject(mail.getSubject());
+		message.setText(mail.getText());
 		return message;
 	}
 
-	private static String getSenderPassword(Properties mailProperties) {
-		return mailProperties.getProperty("mail.credential.password");
+	// private static Message createMessage(String[] recipients, String
+	// mailSubject, String mailText) throws Exception {
+	// logger.debug("Creating the message...");
+	// logger.debug("Retrieving mail properties...");
+	// Properties mailProperties = getMailProperties();
+	// logger.debug("Properties found: " + mailProperties.toString());
+	// String senderMail = getSenderMail(mailProperties);
+	// String senderPassword = getSenderPassword(mailProperties);
+	//
+	// logger.debug("Building message...");
+	// Message message = new MimeMessage(getMailSession(mailProperties));
+	// logger.debug("Setting FROM field with " + senderMail);
+	// message.setFrom(new InternetAddress(senderMail));
+	// logger.debug("Parsing recipients...");
+	// Address[] userMails =
+	// InternetAddress.parse(concatRecipients(recipients));
+	// logger.debug("Setting TO field with " + Arrays.deepToString(userMails));
+	// message.setRecipients(Message.RecipientType.TO, userMails);
+	// logger.debug("Setting SUBJECT field with \"" + mailSubject + "\".");
+	// message.setSubject(mailSubject);
+	// logger.debug("Setting Mail Text with \"" + mailText + "\".");
+	// message.setText(mailText);
+	// logger.debug("Message has been created.");
+	// return message;
+	// }
+
+	private static String getSenderPassword() {
+		return getMailProperties().getProperty("mail.credential.password");
 	}
 
-	private static String getSenderMail(Properties mailProperties) {
-		return mailProperties.getProperty("mail.credential.username");
+	private static String getSenderMail() {
+		return getMailProperties().getProperty("mail.credential.username");
 	}
 
-	private static boolean getDebugMail(Properties mailProperties) {
-		return mailProperties.getProperty("mail.config.debug").equalsIgnoreCase("true") ? true : false;
+	private static boolean getDebugMail() {
+		return getMailProperties().getProperty("mail.config.debug").equalsIgnoreCase("true") ? true : false;
 	}
 
-	private static Properties getMailProperties() throws Exception {
-		return PropertyLoader.loadProperties(PropertyType.MAIL_CONFIGS);
+	private static Properties getMailProperties() {
+		return emailProperties;
 	}
 
-	private static Session getMailSession(Properties mailProperties, final String senderMail, final String senderPassword) {
-		logger.debug("Building mail session with username=" + senderMail + ", password=" + senderPassword + "...");
-		Session session = Session.getDefaultInstance(mailProperties, new javax.mail.Authenticator() {
+	private static Session getMailSession() {
+		logger.debug("Building mail session with username=" + getSenderMail() + ", password=" + getSenderPassword() + "...");
+		Session session = Session.getDefaultInstance(getMailProperties(), new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(senderMail, senderPassword);
+				return new PasswordAuthentication(getSenderMail(), getSenderPassword());
 			}
 		});
-		session.setDebug(getDebugMail(mailProperties));
+		session.setDebug(getDebugMail());
 		logger.debug("Mail session debugging is set to \"" + session.getDebug() + "\".");
 		logger.debug("Mail session has been built.");
 		return session;
