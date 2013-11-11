77bb3a0f41205dbc9c8cbef534c6d18488b6c53b
dvalvezon <danilovalvezon@hotmail.com>
2013-11-11 18:11:46 -0200

    Download logic implemented... Need refactoring, logging and token control for download...

--- ultrashare/src/main/java/com/ultrashare/controller/DownloadController.java
+++ ultrashare/src/main/java/com/ultrashare/controller/DownloadController.java
@@ -13,6 +13,7 @@ import br.com.caelum.vraptor.interceptor.download.InputStreamDownload;
 import com.ultrashare.component.facilities.FTPHandler;
 import com.ultrashare.component.facilities.FTPRetrieveAction;
 import com.ultrashare.component.facilities.Validate;
+import com.ultrashare.component.vo.DownloadConfirmVO;
 import com.ultrashare.dao.ShareDAO;
 import com.ultrashare.model.Share;
 
@@ -20,6 +21,10 @@ import com.ultrashare.model.Share;
 public class DownloadController {
 
 	private static Logger logger = Logger.getLogger(DownloadController.class);
+
+	private static final long MIN_WAIT = 10000L;
+
+	private static final long MAX_WAIT = 120000L;
 
 	private Result result;
 
@@ -43,33 +48,79 @@ public class DownloadController {
 		result.redirectTo(UploadController.class).form();
 	}
 
-	public Share confirm(Share share) {
-		return share;
+	public DownloadConfirmVO confirm(Share share) {
+		return new DownloadConfirmVO(share);
 	}
 
 	// pi -> Share id, pc -> Share confirmationCode
-	public void start(String pi, String pc) {
+	@Post
+	public DownloadConfirmVO start(String pi, String pc) {
 		if (!Validate.ifAnyStringIsNullOrEmpty(pi, pc) && !Validate.ifAnyStringIsNotNumeric(pi, pc)) {
 			Share share = shareDao.find(Long.valueOf(pi));
 			if (share != null && share.getConfirmationCode().equals(Long.valueOf(pc))) {
 				share.addDownloadAttempt();
+				share.generateTimeToken();
 				shareDao.update(share);
-				result.redirectTo(this).download(share);
-				return;
+				return new DownloadConfirmVO(share);
 			}
 		}
 		result.redirectTo(UploadController.class).form();
-	}
-
-	@Get
-	@Path("/download/test/{fileName}")
-	public Download test(String fileName) {
-		return new InputStreamDownload(FTPHandler.processFTPAction(new FTPRetrieveAction(fileName)), "application/octet-stream", "test.txt");
+		return null;
 	}
 
 	@Post
-	private Download download(Share share) {
-		return new InputStreamDownload(FTPHandler.processFTPAction(new FTPRetrieveAction(share.getSharedUpload().getFileName())), share.getSharedUpload()
-				.getFileContentType(), share.getSharedUpload().getFileName(), true, share.getSharedUpload().getFileSize());
+	public Download download(String pid, String pcon) {
+		logger.debug("download method called...");
+		if (!Validate.ifAnyStringIsNullOrEmpty(pid, pcon) && !Validate.ifAnyStringIsNotNumeric(pid, pcon)) {
+			logger.debug("Params ok...");
+			Share share = shareDao.find(Long.valueOf(pid));
+			// Long diffTime;
+			logger.debug("submitting validation: id=" + pid + " code=" + pcon);
+			logger.debug(share.getConfirmationCode().equals(Long.valueOf(pcon)));
+			if (share != null && share.getConfirmationCode().equals(Long.valueOf(pcon))) {
+				logger.debug("passed.");
+				// && (diffTime = System.currentTimeMillis() -
+				// share.getLastTimeToken()) >= MIN_WAIT && diffTime <=
+				// MAX_WAIT) {
+				return new InputStreamDownload(FTPHandler.processFTPAction(new FTPRetrieveAction(share.getSharedUpload().getFileName())),
+						"application/octet-stream", share.getSharedUpload().getFileName(), false, share.getSharedUpload().getFileSize());
+			}
+			logger.debug("verification failure. share code=" + share.getConfirmationCode());
+		}
+		result.forwardTo(UploadController.class).form();
+		return null;
+		// return new InputStreamDownload(FTPHandler.processFTPAction(new
+		// FTPRetrieveAction(fileName)), "application/octet-stream",
+		// "test.txt");
 	}
+
+	// @Get
+	// @Path("/download/download/{pi}/{pc}")
+	// private Download download(String pi, String pc) {
+	// logger.debug("download method called...");
+	// if (!Validate.ifAnyStringIsNullOrEmpty(pi, pc) &&
+	// !Validate.ifAnyStringIsNotNumeric(pi, pc)) {
+	// logger.debug("Params ok...");
+	// Share share = shareDao.find(Long.valueOf(pi));
+	// // Long diffTime;
+	// logger.debug("submitting validation: id=" + pi + " code=" + pc);
+	// logger.debug(share.getConfirmationCode().equals(Long.valueOf(pc)));
+	// if (share != null &&
+	// share.getConfirmationCode().equals(Long.valueOf(pc))) {
+	// logger.debug("passed.");
+	// // && (diffTime = System.currentTimeMillis() -
+	// // share.getLastTimeToken()) >= MIN_WAIT && diffTime <=
+	// // MAX_WAIT) {
+	// return new InputStreamDownload(FTPHandler.processFTPAction(new
+	// FTPRetrieveAction(share.getSharedUpload().getFileName())), share
+	// .getSharedUpload().getFileContentType(),
+	// share.getSharedUpload().getFileName(), false,
+	// share.getSharedUpload().getFileSize());
+	// }
+	// logger.debug("verification failure. share code=" +
+	// share.getConfirmationCode());
+	// }
+	// result.forwardTo(UploadController.class).form();
+	// return null;
+	// }
 }
