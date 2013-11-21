bf9053231877e9890ce7269659f4b7310e0fb8e9
Danilo Valvezon <danilovalvezon@hotmail.com>
2013-11-21 07:10:38 -0200

    Added error() call

--- ultrashare/src/main/java/com/ultrashare/controller/MainController.java
+++ ultrashare/src/main/java/com/ultrashare/controller/MainController.java
@@ -4,16 +4,29 @@ import org.apache.log4j.Logger;
 
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
 
 import com.ultrashare.component.facilities.Log;
+import com.ultrashare.component.facilities.Validate;
 
 @Resource
 public class MainController {
 
 	private static Logger logger = Logger.getLogger(MainController.class);
 
-	public MainController() {
+	private Result result;
 
+	public MainController(Result result) {
+		this.result = result;
+
+	}
+
+	public void error(String throwable) {
+		logger.debug(Log.header("error", Log.entry("throwable", throwable)));
+		if (Validate.ifAnyObjectIsNull(throwable)) {
+			result.redirectTo(UploadController.class).form();
+		}
+		logger.debug(Log.footer("error"));
 	}
 
 	@Get
